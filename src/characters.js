d337904abcad48d6a476f2b28a48c32ad0f6b33d
unknown <brahim.bouargane2000@gmail.com>
2022-04-13 22:25:21 +0000

    First commit for this project

---
+++ src/characters.js
@@ -0,0 +1,4 @@
+export const numbers = "0123456789";
+export const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export const symbols = "!'^+%&/()=?_#$½§{[]}|;:>÷`<.*-@é";
+export const lowerCaseLettters = "abcdefghijklmnopqrstuvwxyz";
